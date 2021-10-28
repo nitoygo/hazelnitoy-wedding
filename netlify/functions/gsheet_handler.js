0cffa546a77484ebee0a5bd69acb4c69c2beea63
Benito Go III <benito.go@paymaya.com>
2021-10-29 00:43:34 +0800

    fixed google sheet handler

--- netlify/functions/gsheet_handler.js
+++ netlify/functions/gsheet_handler.js
@@ -3,53 +3,64 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 // Ensure you've updated this file with your client secret
 const clientSecret = require("./client_secret.json");
 
-// Add your Google sheet ID here
+// Google sheet ID here
 const googleSheetID = "1jyrvbrGkFP62SwAPy16zhdylgknMOP1nUDlbpkqgArM";
 
-// Instantiates the spreadsheet
-const sheet = new GoogleSpreadsheet(googleSheetID);
+// Constants for Column Names
+const NAME = "NAME";
+const CONTACT_NUMBER = "CONTACT NUMBER";
+const CONFIRMED_SEAT = "CONFIRMED SEAT";
+
+// Instantiates the spreadsheet document
+const doc = new GoogleSpreadsheet(googleSheetID);
 
 async function updateData(body) {
   try {
     let data = {
-      "NAME": body.name,
-      "CONTACT NUMBER": body.contactNumber,
-      "CONFIRMED SEAT": body.count
+      [NAME]: body.name,
+      [CONTACT_NUMBER]: body.contactNumber,
+      [CONFIRMED_SEAT]: body.count
     };
 
     // Authenticate using the JSON file we set up earlier
-    await sheet.useServiceAccountAuth(clientSecret);
-    await sheet.loadInfo();
+    await doc.useServiceAccountAuth(clientSecret);
+    await doc.loadInfo();
 
-    // Get the first tab's data
-    const tab = sheet.sheetsByIndex[0];
+    // Get the first sheet
+    const sheet = doc.sheetsByIndex[0];
+    console.info(sheet.title);
+    console.info(sheet.rowCount);
 
     // Get row data
-    const rows = await tab.getRows();
+    const rows = await sheet.getRows();
 
-    var index = 0;
     // If we have data
     if (rows.length > 0) {
-      // Iterate through the array of rows
-      // and push the clean data from your spreadsheet
-      rows.forEach((row, i) => {
-        var name = row["NAME"];
-        if (name == data["NAME"]) {
-          // set values to each cell
-          row["CONTACT NUMBER"] = data["CONTACT NUMBER"];
-          row["CONFIRMED SEAT"] = data["CONFIRMED SEAT"];
-          index = i;
-        }
+      var matchedRow = rows.find(row => {
+        return row[NAME] == data[NAME];
       });
-    } else {
-      console.log("[WARNING!] Google Spreadsheet is empty.");
-    }
 
-    // Save updated row
-    await rows[index].save();
+      if (matchedRow) {
+        // if found match, update existing row
+        matchedRow[CONTACT_NUMBER] = data[CONTACT_NUMBER];
+        matchedRow[CONFIRMED_SEAT] = data[CONFIRMED_SEAT];
+
+        await matchedRow.save();
+      } else {
+        // if none match, add new row
+        var addedRow = await sheet.addRow({
+          [NAME]: data[NAME],
+          [CONTACT_NUMBER]: data[CONTACT_NUMBER],
+          [CONFIRMED_SEAT]: data[CONFIRMED_SEAT]
+        });
+
+        await addedRow.save();
+      }
+    } else {
+      console.warn("[WARNING!] Google Spreadsheet is empty.");
+    }
   } catch (err) {
-    console.log(err);
-    return false;
+    console.error(err);
   }
 }
 
